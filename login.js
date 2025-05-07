c38298f700842f0a57314ee1aac9a96ffc08708b
nghia <nguyenvietnghia24092006@gmail.com>
2025-05-07 14:53:25 +0700

    add html, css, js

---
+++ login.js
@@ -0,0 +1,21 @@
+// function isEmail() {
+//     var text = document.querySelector('#email').value.trim()
+//     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+//     return emailRegex.test(text);
+// }
+
+// function checkAccount() {
+//     if (!isEmail()) {
+//         alert('hello')
+//         document.querySelector('#error_email').style.display = 'block'
+//     }
+//     else {
+//         alert('fucvk')
+//     }
+// }
+function checkAccount() {
+    var text = document.querySelector('#email').value.trim()
+    var password = document.querySelector('#password').value
+
+    
+}
