b4fc3a449dd77d193d23330de8beb4cfbd8ed0eb
nghia <nguyenvietnghia24092006@gmail.com>
2025-05-08 00:11:29 +0700

    update create new account

--- login.js
+++ login.js
@@ -1,21 +1,86 @@
-// function isEmail() {
-//     var text = document.querySelector('#email').value.trim()
-//     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-//     return emailRegex.test(text);
-// }
+var account = JSON.parse(localStorage.getItem('account')) || {
+    'adminAccount@gmail.com': 'password123'
+}
 
-// function checkAccount() {
-//     if (!isEmail()) {
-//         alert('hello')
-//         document.querySelector('#error_email').style.display = 'block'
-//     }
-//     else {
-//         alert('fucvk')
-//     }
-// }
+const inputEmail = document.querySelector('#email')
+const inputPassword = document.querySelector('#password')
+const errorEmail = document.querySelector('#error_email')
+const errorAccount = document.querySelector('#email_password_incorrect')
+
+function isEmail() {
+    const text = document.querySelector('#email').value.trim()
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return emailRegex.test(text);
+}
+
 function checkAccount() {
-    var text = document.querySelector('#email').value.trim()
-    var password = document.querySelector('#password').value
+    if (!isEmail()) {
+        errorEmail.style.display = 'block'
+    } else {
+        const inputEmail = document.querySelector('#email')
+        const inputPassword = document.querySelector('#password')
+        const errorEmail = document.querySelector('#error_email')
+        const errorAccount = document.querySelector('#email_password_incorrect')
 
-    
+        const email = inputEmail.value.trim()
+        const password = inputPassword.value
+
+        inputEmail.value = ''
+        inputPassword.value = ''
+
+        if (account[email] === password) {
+            errorEmail.style.display = 'none'
+            errorAccount.style.display = 'none'
+            alert('Login successfully')
+        } else {
+            errorAccount.style.display = 'block'
+        }
+    }
+}
+
+function changeDisplay() {
+    const displayA = document.querySelectorAll('.display_a')
+    const displayB = document.querySelector('.display_b')
+
+    if (displayB.style.display === 'none') {
+        displayB.style.display = 'block'
+        displayA.forEach(function (x) {
+            x.style.display = 'none'
+        })
+    } else {
+        displayA.forEach(function (x) {
+            x.style.display = 'block'
+        })
+        displayB.style.display = 'none'
+    }
+}
+
+function addAccount() {
+    if (!isEmail()) {
+        errorEmail.style.display = 'block'
+    } else {
+        const inputEmail = document.querySelector('#email')
+        const inputPassword = document.querySelector('#password')
+        const errorEmail = document.querySelector('#error_email')
+        const errorAccount = document.querySelector('#email_password_incorrect')
+
+        const email = inputEmail.value.trim()
+        const password = inputPassword.value
+
+        inputEmail.value = ''
+        inputPassword.value = ''
+
+        if (Object.keys(account).indexOf(email) === -1) {
+            errorEmail.style.display = 'none'
+            account[email] = password
+            alert("create a new account successfully")
+        } else {
+            errorAccount.style.display = 'block'
+        }
+    }
+    localStorage.setItem('account', JSON.stringify(account));
+}
+
+function resetAccount() {
+    localStorage.removeItem('account');
 }
